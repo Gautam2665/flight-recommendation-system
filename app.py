ce15bb91bb185061ec04cfd74bfa5dd9b6b0f93a
Gautam <gautammulay123@gmail.com>
2025-12-19 11:09:17 +0530

    port fix

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ def flight_details():
 
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
